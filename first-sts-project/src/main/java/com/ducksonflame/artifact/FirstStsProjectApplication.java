d959035b1c9782fca64a7658a0f73439dd938a14
DucksOnFlame <bartlomiej.styczynski@gmail.com>
2017-03-03 22:36:17 +0100

    First files
    Adding first files...

---
+++ first-sts-project/src/main/java/com/ducksonflame/artifact/FirstStsProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ducksonflame.artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStsProjectApplication.class, args);
+	}
+}
